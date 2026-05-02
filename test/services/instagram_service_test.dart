572b9e35e5c3243cad356996bf98a041af288dcd
di3go04 <mclavijorojas@gmail.com>
2026-05-02 13:29:12 -0500

    test: add 8 unit tests for InstagramService.normalizeUrl (#3)

---
+++ test/services/instagram_service_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:insta_grab/services/instagram_service.dart';
+
+void main() {
+  group('InstagramService.normalizeUrl', () {
+    test('1. Debería retornar la URL canónica estándar', () {
+      expect(InstagramService.normalizeUrl('https://www.instagram.com/p/ABC123/'), 
+             'https://www.instagram.com/p/ABC123/');
+    });
+
+    test('2. Debería añadir "www" si falta', () {
+      expect(InstagramService.normalizeUrl('https://instagram.com/p/ABC123/'), 
+             'https://www.instagram.com/p/ABC123/');
+    });
+
+    test('3. Debería convertir /reel/ a /p/', () {
+      expect(InstagramService.normalizeUrl('https://www.instagram.com/reel/ABC123/'), 
+             'https://www.instagram.com/p/ABC123/');
+    });
+
+    test('4. Debería convertir /tv/ a /p/', () {
+      expect(InstagramService.normalizeUrl('https://www.instagram.com/tv/ABC123/'), 
+             'https://www.instagram.com/p/ABC123/');
+    });
+
+    test('5. Debería limpiar parámetros de consulta (query params)', () {
+      expect(InstagramService.normalizeUrl('https://www.instagram.com/p/ABC123/?utm_source=share'), 
+             'https://www.instagram.com/p/ABC123/');
+    });
+
+    test('6. Debería limpiar fragmentos (#)', () {
+      expect(InstagramService.normalizeUrl('https://www.instagram.com/p/ABC123/#fragment'), 
+             'https://www.instagram.com/p/ABC123/');
+    });
+
+    test('7. Debería retornar null para URLs que no son de Instagram', () {
+      expect(InstagramService.normalizeUrl('https://twitter.com/user/status/123'), isNull);
+    });
+
+    test('8. Debería retornar null para strings vacíos', () {
+      expect(InstagramService.normalizeUrl(''), isNull);
+    });
+  });
+}
